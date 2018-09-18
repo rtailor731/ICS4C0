1cdb7d7b9674b1768460e820f021a6199129310c
rtailor731 <36168126+rtailor731@users.noreply.github.com>
2018-09-18 09:25:53 -0400

    Create HellowWorldSWT.java

---
+++ Topic B Programming Skills/HellowWorldSWT.java
@@ -0,0 +1,35 @@
+package org.eclipse.swt.examples.helloworld; 
+ 
+import java.util.ResourceBundle; 
+ 
+import org.eclipse.swt.SWT; 
+import org.eclipse.swt.layout.FillLayout; 
+import org.eclipse.swt.widgets.Display; 
+import org.eclipse.swt.widgets.Label; 
+import org.eclipse.swt.widgets.Shell; 
+ 
+/*
+ * This example builds on HelloWorld2 and demonstrates how to resize the Label when the Shell resizes using a Layout. 
+ */ 
+public class HelloWorld4 { 
+ private static ResourceBundle resHello = ResourceBundle.getBundle("examples_helloworld"); 
+ 
+ public static void main(String[] args) { 
+  Display display = new Display(); 
+  Shell shell = new HelloWorld4().open(display); 
+  while (!shell.isDisposed()) 
+   if (!display.readAndDispatch()) 
+    display.sleep(); 
+  display.dispose(); 
+ } 
+ 
+ public Shell open(Display display) { 
+  Shell shell = new Shell(display); 
+  shell.setLayout(new FillLayout()); 
+  Label label = new Label(shell, SWT.CENTER); 
+  label.setText(HelloWorld4.resHello.getString("Hello_world")); 
+  shell.pack(); 
+  shell.open(); 
+  return shell; 
+ } 
+}
