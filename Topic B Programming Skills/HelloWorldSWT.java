0b1e2c683ec881c49fbf1166415ecd1606511eb6
rtailor731 <36168126+rtailor731@users.noreply.github.com>
2018-09-19 11:43:16 -0400

    Update HelloWorldSWT.java

--- Topic B Programming Skills/HelloWorldSWT.java
+++ Topic B Programming Skills/HelloWorldSWT.java
@@ -5,24 +5,19 @@ import org.eclipse.swt.widgets.Text;
 
 public class SWTHelloWorld {
 
-private static ResourceBundle resHello = ResourceBundle.getBundle("examples_helloworld"); 
- 
- public static void main(String[] args) { 
-  Display display = new Display(); 
-  Shell shell = new HelloWorld2().open(display); 
-  while (!shell.isDisposed()) 
-   if (!display.readAndDispatch()) 
-    display.sleep(); 
-  display.dispose(); 
- } 
- 
- public Shell open(Display display) { 
-  Shell shell = new Shell(display); 
-  shell.setLayout(new FillLayout()); 
-  Label label = new Label(shell, SWT.CENTER); 
-  label.setText(HelloWorld2.resHello.getString("Hello_world")); 
-  shell.pack(); 
-  shell.open(); 
-  return shell; 
- } 
+public static void main (String [] args) {
+	Display display = new Display ();
+	Shell shell = new Shell(display);
+	
+	Text helloWorldTest = new Text(shell, SWT.NONE);
+	helloWorldTest.setText("Hello World SWT");
+	helloWorldTest.pack();
+	
+	shell.pack();
+	shell.open ();
+	while (!shell.isDisposed ()) {
+		if (!display.readAndDispatch ()) display.sleep ();
+	}
+	display.dispose ();
+}
 }
