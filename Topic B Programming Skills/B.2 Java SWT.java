ab2d061d0e2696913f069c41ea7601b43da359ee
Rahul Tailor <36168126+rtailor731@users.noreply.github.com>
2018-10-26 09:17:11 -0400

    Create B.2 Java SWT.java

---
+++ Topic B Programming Skills/B.2 Java SWT.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int num, i, fact=1;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter a Number : ");
+        num = scan.nextInt();
+		
+        for(i=num; i>0; i--)
+        {
+            fact = fact*i;
+        }
+		
+        System.out.print("Factorial of " + num + " is " + fact);
+    }
+}
